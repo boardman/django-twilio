5d97f3cce7323ad6ec0b9f1b2ce445d58f673863
Roger Boardman <roger@bxthree.com>
2017-06-21 13:19:45 +0100

    Update imports and method signatures for compatability with twilio-python 6.x

--- django_twilio/views.py
+++ django_twilio/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse
 
 from .decorators import twilio_view
 
@@ -20,7 +21,7 @@ Usage::
         # ...
     )
     """
-    r = twiml.Response()
+    r = VoiceResponse()
     r.say(text, voice=voice, language=language, loop=loop)
     return r
 
@@ -41,7 +42,7 @@ def play(request, url, loop=None):
             # ...
         )
     """
-    r = twiml.Response()
+    r = VoiceResponse()
     r.play(url, loop=loop)
     return r
 
@@ -61,9 +62,9 @@ def gather(request, action=None, method='POST', num_digits=None, timeout=None,
             # ...
         )
     """
-    r = twiml.Response()
-    r.gather(action=action, method=method, numDigits=num_digits,
-             timeout=timeout, finishOnKey=finish_on_key)
+    r = VoiceResponse()
+    r.gather(action=action, method=method, num_digits=num_digits,
+             timeout=timeout, finish_on_key=finish_on_key)
     return r
 
 
@@ -83,11 +84,11 @@ def record(request, action=None, method='POST', timeout=None,
             # ...
         )
     """
-    r = twiml.Response()
+    r = VoiceResponse()
     r.record(action=action, method=method, timeout=timeout,
-             finishOnKey=finish_on_key, maxLength=max_length,
-             transcribe=transcribe, transcribeCallback=transcribe_callback,
-             playBeep=play_beep)
+             finish_on_key=finish_on_key, max_length=max_length,
+             transcribe=transcribe, transcribe_callback=transcribe_callback,
+             play_beep=play_beep)
     return r
 
 
@@ -109,9 +110,9 @@ def sms(request, message, to=None, sender=None, action=None, method='POST',
             # ...
         )
     """
-    r = twiml.Response()
-    r.message(msg=message, to=to, sender=sender, method='POST', action=action,
-              statusCallback=status_callback)
+    r = MessagingResponse()
+    r.message(body=message, to=to, sender=sender, method='POST', action=action,
+              status_callback=status_callback)
     return r
 
 
@@ -134,8 +135,8 @@ def message(request, message, to=None, sender=None, action=None,
         )
     """
 
-    r = twiml.Response()
-    r.message(msg=message, to=to, sender=sender, method='POST',
+    r = MessagingResponse()
+    r.message(body=message, to=to, sender=sender, method='POST',
               action=action, statusCallback=status_callback,
               media=media)
 
@@ -157,15 +158,15 @@ def dial(request, number, action=None, method='POST', timeout=None,
             # ...
         )
     """
-    r = twiml.Response()
+    r = VoiceResponse()
     r.dial(number=number, action=action, method=method, timeout=timeout,
-           hangupOnStar=hangup_on_star, timeLimit=time_limit,
-           callerId=caller_id)
+           hangup_on_star=hangup_on_star, time_limit=time_limit,
+           caller_id=caller_id)
     return r
 
 
 @twilio_view
-def conference(request, name, muted=None, beep=None,
+def conference(request, number, name, muted=None, beep=None,
                start_conference_on_enter=None, end_conference_on_exit=None,
                wait_url=None, wait_method='POST', max_participants=None):
     """
@@ -181,10 +182,10 @@ Usage::
         # ...
     )
     """
-    r = twiml.Response()
-    r.dial().conference(name=name, muted=muted, beep=beep,
-                        startConferenceOnEnter=start_conference_on_enter,
-                        endConferenceOnExit=end_conference_on_exit,
-                        waitUrl=wait_url, waitMethod=wait_method,
-                        )
+    r = VoiceResponse()
+    r.dial(number=number).conference(name=name, muted=muted, beep=beep,
+                                     startConferenceOnEnter=start_conference_on_enter,
+                                     endConferenceOnExit=end_conference_on_exit,
+                                     waitUrl=wait_url, waitMethod=wait_method,
+                                    )
     return r
